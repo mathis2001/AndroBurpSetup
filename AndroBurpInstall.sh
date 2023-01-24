ab46b7639177d4dea3d77d5fdb5f626957827c0c
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2023-01-24 13:56:23 +0100

    Update AndroBurpInstall.sh

--- AndroBurpInstall.sh
+++ AndroBurpInstall.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Assign the location of the Burp Suite certificate to a variable
-CERT="9a5ba575.0"
 
-# Check if ADB is installed
+CERT="path/to/burp/certificate"
+
 if ! [ -x "$(command -v adb)" ]; then
   echo 'Error: ADB is not installed.'
   sudo apt install adb
 fi
 
 if [ ! -f $CERT ]; then
-  echo "Error: Burp Suite certificate not found at $CERT_LOCATION" >&2
+  echo "Error: Burp Suite certificate not found at $CERT" >&2
   exit 1
 fi
 
@@ -22,4 +21,3 @@ adb remount
 adb shell "mv /sdcard/$CERT /system/etc/security/cacerts && chmod 644 /system/etc/security/cacerts/$CERT"
 
 echo "Burp Suite certificate installed successfully!"
-
